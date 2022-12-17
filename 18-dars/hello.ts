b4363bd6fae4a64a6e0c7a0d9a9519dcb5e3065a
mirzobeck <sobirovmirzobek3@gmail.com>
2022-12-17 12:32:41 +0500

    djdhudhu

---
+++ 18-dars/hello.ts
@@ -0,0 +1,55 @@
+//data types
+//45,67,-184,3.14 -> number
+//'math', "text", `tilda` -> string
+
+//let a  = 15;
+//let S  = a*a;
+//console.log('Площадь:'+S)
+//let a: number=15
+//let b: number=25
+//let S: number=a*b
+//let P:=2*(a+b)
+//console.log('square',S , 'perimmetr',P)
+
+//let d: number=20
+//let L: number=Math.PI*d
+//console.log('Line',L)
+
+//let a: number=20
+//let v: number=a**3
+//let s: number=6*(a*a)
+//console.log("V",v ,"S",s)
+
+//let a: number=10
+//let b: number=15
+//let c: number=20
+//let v: number=a*b*c
+//let s: number=2*(a*b+b*c+a*c)
+//console.log("V",v,"S",s)
+
+//let r: number=30
+//let l: number=2*Math.PI*r
+//let s: number=Math.PI*(r*r)
+//console.log("S",s,"L",l)
+
+//let a: number=20
+//let b: number=30
+//let g: number=(a+b)/2
+//console.log('G',g)
+
+//let a: number=6
+//let b: number=6
+//console.log(Math.sqrt(a*b))
+
+//let a: number=25
+//let b: number=25
+//console.log(g=a+b,f=a-b,d=a*b,Math.sqrt(a)/Math.sqrt(b))
+
+//let a: number=25
+//let b: number=25
+//console.log(g=a+b,f=a-b,d=a*b,h=a/b);
+
+let a: number=4
+let b: number=5
+console.log(Math.sqrt(a*a)+Math.sqrt(b*b),a+b+9);
+
