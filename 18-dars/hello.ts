8e1f88bf97fa1c5379ce030db2d889085a7224c2
mirzobeck <sobirovmirzobek3@gmail.com>
2022-12-20 12:21:04 +0500

    nohiwfdquhqq

--- 18-dars/hello.ts
+++ 18-dars/hello.ts
@@ -49,7 +49,134 @@
 //let b: number=25
 //console.log(g=a+b,f=a-b,d=a*b,h=a/b);
 
-let a: number=4
-let b: number=5
-console.log(Math.sqrt(a*a)+Math.sqrt(b*b),a+b+9);
+//let a: number=4
+//let b: number=5
+//console.log(Math.sqrt(a*a)+Math.sqrt(b*b),a+b+9);
+//17//
+//let a: number=25
+//let b: number=20
+//let c: number=15
+//let ac:number=Math.abs(a-c)
+//let bc:number=Math.abs(b-c)
+//let  S=ac+bc
+//console.log(ac,bc,S);
+//18//
+//let a: number=10;
+//let c: number=15;
+//let b: number=20;
+//let ac:number=Math.abs(c-a);
+//let bc:number=Math.abs(b-c);
+//let S: number=ac+bc;
+//console.log(ac,bc,S);
+//19//
+
+let x=15/4
+let y=43%4
+
+//
+//let a=3**5
+//let b=Math.sqrt(49)
+//let b=49**1/4
+
+//x; y; a; b 
+
+//Math
+//console.log(Math.floor(1.26));
+//console.log(Math.round(1.76));
+//console.log(Math.ceil(1.000001));
+
+//
+
+
+//1 masala
+
+//let L=235
+//let M=Math.floor(L/100);
+//console.log(L+ 'sm da'  + M+ `to'liq metr`)
+
+//2 masala
+
+//let K=2300
+//let T=Math.floor(K/1000);
+//console.log(K+"kg da", +  T + "To`liq tonna");
+
+//3 masala
+//let B=2048
+//let K=Math.floor(B/1024);
+//console.log(B+" bayt da ",+K+" To`liq killobayt ");
+
+//4 masala
+//let a=40
+//let b=20
+//console.log(a/b);
+
+//5 masala
+//let a=45;
+//let b=20;
+//console.log(a % b);
+
+//6 masala
+//let a=34;
+//console.log(Math.floor(a/10),(a%10));
+
+//7 masala
+//let a=45
+//let b=Math.floor(a/10)+(a%10)
+//let c=Math.floor(a/10)*(a%10)
+//console.log(b,c);
+
+//8 masala
+//let a=45
+//let b=Math.floor(a/10)
+//let c=(a%10)*10
+//console.log(c+b);
+
+//9 masala
+//let a=350
+//console.log(Math.floor(a/100));
+
+//10 masala
+//let a=350
+//console.log(Math.floor(a%10),Math.floor(a/70));
+
+//11 masala
+//let a=456
+//let b=Math.floor(a/100)
+//let c=Math.floor((a%100)/10)
+//let d=a%10
+//console.log(b+c+d);
+
+//12 masala
+//let a=456
+//let b=Math.floor(a/100)
+//let c=Math.floor((a%100)/10)
+//let d=a%10
+//console.log(d*100+c*10+b);
+
+//13 masala
+//let a=456
+//let b=Math.floor(a/100)
+//let c=Math.floor((a%100)/10)
+//let d=a%10
+//console.log(c*100+d*10+b);
+
+//14 masala
+//let a=456
+//let b=Math.floor(a/100)
+//let c=Math.floor((a%100)/10)
+//let d=a%10
+//console.log(d*100+b*10+c);
+
+//15 masala
+//let a=456
+//let b=Math.floor(a/100)
+//let c=Math.floor((a%100)/10)
+//let d=a%10
+//console.log(c*100+b*10+d);
+
+
+
+
+
+ 
 
---
+++ 18-dars/prompt.js
@@ -0,0 +1,5 @@
+let prompt=require('prompt-sync')()
+
+let x=prompt('x niu kiriting')
+
+console.log(x**2);
