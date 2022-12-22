9d1cdf1993cc354a4c5280aceb86f9202a86b2c3
mirzobeck <sobirovmirzobek3@gmail.com>
2022-12-22 12:17:39 +0500

    iii

---
+++ 19-dars/main.js
@@ -0,0 +1,101 @@
+//let k=147;
+//let d=k%7
+//console.log(d);
+
+//number + number =number
+
+//let n = 5 + 6
+//console.log(type of);
+
+// Compare and conditions
+// 1 masala
+//let a = 80
+//console.log(a > 0 ? 'Положительное' : 'Отрицательное');
+
+// 2 masala
+let a = 50;
+let b = 45;
+let c = 30;
+let d = 600
+
+//console.log(a % 2 ===1 ? 'Нечетное число' : 'Четное число');
+
+//3 masala
+//console.log(a%2 ===1);
+
+//4 masala
+//console.log(a>2 && b<=3);
+
+//5 masala
+//console.log(a>=0 || b<-2);
+
+//6 masala
+//console.log(a<b && b<c);
+
+//7 masala
+//console.log((a<b && b<c) || (a>b && b>c) ? "b находится между а и с" : "либо нет");
+
+//8 masala
+//console.log(a % 2 ==1 && b % 2 == 1);
+
+//9 masala
+//console.log(a % 2 ==1 || b % 2 == 1);
+
+//10 masala
+//console.log((a % 2 ==1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1));
+
+//11 masala
+//console.log((a % 2 ==1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0));
+
+//12 masala
+//console.log(a>0 && b>0 && c>0);
+
+//13 masala
+//console.log(a>0 || b>0 || c>0);
+
+//14 masala
+//console.log((a > 0 && b<=0 && c <=0 || a <= 0 && b > 0 && c <= 0 || a <= 0 && b <= 0 && c > 0));
+
+//15 masala
+//console.log((A==(-1)*B)|(A==(-1)*C)|(B==(-1)*C));
+
+//16 masala
+//console.log(a >9 && a <100 && a % 2 == 0);
+
+//17 masala
+//console.log(d >99 && d <999 && d % 2 == 0);
+
+//18 masala
+//console.log(d > 99 && d < 999 && d % 2 == 1);
+
+//19 masala
+//console.log((a==(-1)*b)||(a==(-1)*c)||(b==(-1)*c));
+
+//20 masala
+//let q = 321
+//let w = Math.floor(q/100)
+//let e = Math.floor((q%100)/10)
+//let r = Math.floor((q%100)%10)
+//console.log( w === e && e === r && r === w );
+
+//21 masala
+//let q = 321
+//let w = Math.floor(q/100)
+//let e = Math.floor((q%100)/10)
+//let r = Math.floor((q%100)%10)
+//console.log(  w < e && e < r );
+
+//22 masala
+//let q = 321
+//let w = Math.floor(q/100)
+//let e = Math.floor((q%100)/10)
+//let r = Math.floor((q%100)%10)
+//console.log(  w < e && e < r || w > e && e > r);
+
+//23 masala
+//let q = 2112
+//let w = Math.floor(q/1000)
+//let e = Math.floor((q%1000)/100)
+//let r = Math.floor((q%100)/10)
+//let t = Math.floor((q%100)%10)
+//console.log(w === t && e === r);
