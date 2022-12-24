9af77676bd365874a5d155779cd91be8cc466142
mirzobeck <sobirovmirzobek3@gmail.com>
2022-12-24 12:52:42 +0500

    djkdjj

---
+++ 20-dars/main.js
@@ -0,0 +1,152 @@
+// + - * / %
+// string + number = string // concat
+// number + number = number // arifmetic
+
+// Booleans
+// Compare
+// < > <= >= == != === !==
+// number > number = boolean
+
+//conditionals
+// && ||
+
+// boolean && boolean = boolean
+
+
+//1 masala
+
+let a = 30
+let b = 15
+let c = 70
+let x = 0
+let y = 0
+//if(a<0)
+//console.log(a+1);
+//else
+//console.log(a);
+
+//2 masala
+//if ( a>0 )
+//console.log(a+1);
+//else console.log(a-2);
+
+//3 masala
+//if ( a > 0){
+    //a = a + 1
+//}
+//else if (a < 0){
+//a = a - 2
+//}
+//else {
+    //a=10
+//}
+//console.log(a);
+
+//4 masala
+///if( a>0 && b>0 && c>0, ){
+   // console.log( a, b, c);
+//} 
+ //else
+ //console.log(false);
+
+ // 5 masala
+//if ( a>0){
+  //x++
+//}
+//else{
+    //y++
+//} 
+//if( b>0){
+   // x++
+//}
+//else{
+  //  y++
+//}
+//if( c>0){
+    //x++
+//}
+//else{
+  //  y++
+//}
+//console.log("Количество положительных чисел",x,"Количество отрицательных чисел",y);
+
+// 6 masala
+//if( a>b)
+//console.log(a);
+//else
+//console.log(b);
+ 
+// 7 masala
+//if( a>b)
+//console.log(1);
+//else
+//console.log(2);
+
+// 8 masala
+//if( a>b)
+//console.log(a,b);
+//else
+//console.log(b,a);
+
+// 9 masala
+// if ( a > b){
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
+    
+//     console.log(a,b);
+// }
+
+// 10 masala
+// if ( a > b || a < b){
+//     x=a+b;
+//     y=a+b;
+//     console.log(x,y);
+// }
+// else{
+//     x=a*0
+//     y=b*0
+//     console.log(x,y);
+// }
+
+// 11 masala
+// if ( a > b ){
+//         x=a+a;
+//         y=b+a;
+//         console.log(x,y);
+//     }
+//     else if (a<b) {
+//         x=a+b
+//         y=b+a
+//         console.log(x,y);
+//     }else{
+//         x=a*0
+//         y=b*0
+//         console.log(x,y);
+//     }
+
+// 12 masala
+//  if(a<b && a<c)
+//  console.log(a);
+//  else if(b<a && b<c)
+//  console.log(b);
+//  else
+//  console.log(c);
+
+// 13 masala
+// if( a>b && a<c)
+// console.log(a);
+// else if(b>a && b<c)
+// console.log(b);
+// else
+// console.log(c);
+
+//15 masala
+// if( a>b && b>c)
+// console.log(a+b);
+// else if(b>a && a>c)
+// console.log(b+a);
+// else if(c>a && a>b)
+// console.log(a+c);
+// else
+// console.log(false);
