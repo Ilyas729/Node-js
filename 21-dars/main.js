499a8a534c9c48177c9f869faa226501c2fe381a
mirzobeck <sobirovmirzobek3@gmail.com>
2022-12-27 13:06:17 +0500

    dkjjkfj

---
+++ 21-dars/main.js
@@ -0,0 +1,105 @@
+// loops
+
+// For, While, Do While
+
+//1 masala
+// let k = 15 
+// let n = 3
+
+// for (let i=0; i<3; i=i+1){
+//     console.log(k);
+// }
+
+//2 masala
+//  let a = 5
+//  let b = 10
+//  let n = 0
+// for( let i = 5; i<=10; i++){
+
+//     console.log(i);
+//     n++
+
+// }
+// console.log(n);
+
+//3 masala
+//  for (let i = b-1; i>5; --i){
+//      console.log(i);
+//      n++
+//  }
+//     console.log(n);
+
+//4 masala
+// let price = 50_00
+// for (let i = 1; i <= 10; i=i+1) {
+//     console.log(price*i);
+
+// }
+
+//5 masala
+//let price = 50_000 // 1 kg
+
+// for (let i = 0.1; i < 1; i=i+0.1){
+//     console.log(price*i);
+// }
+
+//6 masala
+//  for( let i = 1.1; i <= 2; i=i+0.1){
+//     console.log(price*i);
+//  }
+
+//7 masala
+//  let a = 40
+//  let b = 50
+//  let s = 0
+//  for( let i = 40; i<=50; i++ ){
+//     s=s+i
+//  }
+//  console.log(s);
+
+//8 masala
+//  let a = 40
+//  let b = 50
+//  let s = 1
+//  for( let i=40; i<=50; i++){
+//     s=s*i
+//  }
+//  console.log(s);
+
+//9 masala
+// let a = 40
+// let b = 50
+// let s = 0
+// for( let i=40; i<=50; i++){
+//      s=s+Math.pow(i,2)
+//   }
+//   console.log(s);
+//10 masala
+// let s = 0
+// let n = 5
+// for(let i = 1; i<=n; i++){
+//     console.log(i);
+//     console.log(1/i);
+//     s=s + 1/i
+//     console.log(s);
+// }
+// console.log(s);
+
+// 11 masala
+// let n =10
+// let S = 0
+// for ( let i=0; i<=n; i++ ){
+//     S=S+Math.pow(n+i,2);
+// }
+// console.log(S);
+
+//12 masala
+// let n = 2
+// let k = 1
+// for (let i = 1; i <= n; i += 0.1) {
+//     k *= i;
+//     console.log(i.toFixed(i));
+// }
+// console.log(k);
+
+//13 masala
