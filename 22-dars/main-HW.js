ecbb14f904338afc0312d2a2fb452afc24665d65
mirzobeck <sobirovmirzobek3@gmail.com>
2022-12-29 12:47:10 +0500

    jdjdj

---
+++ 22-dars/main-HW.js
@@ -0,0 +1,33 @@
+//19 masala
+// let n = 6
+// let a = 1
+// for(let i=1; i<=n; i++){
+//     a*=i
+//     console.log(a);
+// }
+
+
+
+//20 masala
+// let a = 1
+// let n = 8
+// let s=0
+// for(let i=1; i<=n; i++){
+//   a*=i 
+//   //console.log(a);
+//   s+=a
+// }
+// console.log(s);
+
+// let n =3 
+// let s =0
+// let b =1
+// for(let i=1; i<=n; i+=1){
+//     let res = (1+0.1*i)*b
+//     s=s+res
+//     b*=-1
+// }
+// console.log(s);
+
+let n = 3
+let k = 2
---
+++ 22-dars/switch.js
@@ -0,0 +1,49 @@
+// let n = 6
+// switch (n) {
+//     case 1:
+//         console.log('bir');
+//         break;
+//     case 2:
+//         console.log('Ikki');
+//         break;
+//     case 3:
+//         console.log('Uch');
+//         break;
+//     case 4:
+//         console.log('To`rt');
+//         break;
+//     case 5:
+//         console.log('Besh');
+//         break
+//         default:
+//             console.log('Boshqa barmoq qolmadi');    
+
+// }
+
+// // 1masala
+// let b = 4
+// switch (b) {
+//     case 1:
+//         console.log('Ponedelnik');
+//         break;
+//     case 2:
+//         console.log('Vtornik');
+//         break;
+//     case 3:
+//         console.log('Sreda');
+//         break;
+//     case 4:
+//         console.log('Chetwerk');
+//         break;
+//     case 5:
+//         console.log('Pyatnica');
+//         break;
+//     case 6:
+//         console.log('Subotta');
+//         break;
+//     case 7: 
+//         console.log('Voskresenya');   
+//     default:
+//         console.log('Bunday kun yo`q');
+        
+// }
