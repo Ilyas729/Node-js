72b3e5da974cd697eaca5acd4cc40c2353bfb4d8
mirzobeck <sobirovmirzobek3@gmail.com>
2023-01-05 13:06:26 +0500

    sjsjjhs

---
+++ 23-dars/main.js
@@ -0,0 +1,39 @@
+//8 masala
+// let d = 24
+// let m = 4
+
+// d=d-1
+// console.log(d);
+
+// if(d==0){
+//     m=m-1
+
+//     console.log(m);
+
+//     switch(m){
+//         case 1:
+//         case 3:
+//         case 5:
+//         case 7:
+//         case 8:
+//         case 10:
+//         case 12:
+//             d=31;
+//             break;
+//         case 4:
+//         case 6:
+//         case 9:
+//         case 11:
+//             d=30;
+//             break;
+//         case 2:
+//             d=28;                            
+//     }
+// }
+
+// console.log(m, d);
+
+//10 masala
+
+// let y = 's'
+// let k = 1
---
+++ 23-dars/series.js
@@ -0,0 +1,192 @@
+const prompt = require('prompt-sync')()
+
+//1.
+// let summ = 0
+
+// for( let i=0; i<10; i++){
+// const x = +prompt('Son kiriting: ')
+// summ = summ+x
+// }
+
+// console.log(summ);
+
+//3 masala
+// let summ = 0
+
+// for( let i=0; i<10; i++){
+// const x = +prompt('Son kiriting: ')
+// summ = (summ+x);
+// }
+
+// console.log(summ / 10);
+
+//4 masala
+// let n = 10
+// let summ = 0
+// let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// summ = (summ+x);
+// p=x*p
+// }
+
+// console.log(summ, p);
+
+//5 masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+//7 masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// summ = (summ+Math.round(x));
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+// 8 masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x%2 ==0){
+//     console.log(x);
+//     summ+=1
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+//9 masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x%2 ==1){
+//     console.log(x);
+//     summ+=1
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+//10 masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x>0){
+//     console.log(true);
+//     // summ+=1
+// }
+// else{
+//     console.log(false);
+
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+//11 masala
+// let n = 10
+// let k =15
+// //let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x<k){
+//     console.log(true);
+//     //summ+=1
+// }else{
+//     console.log(false);
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// // console.log(summ);
+
+//12masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x!=0){
+//     console.log(x);
+//     summ+=1
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+//13 masala
+// let n = 10
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x%2 ==0 && x!=0 && x>0){
+//     console.log(x);
+//     summ=summ+x
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+// console.log(summ);
+
+//14 masala
+// let n = 10
+// let k =15
+// let summ = 0
+// //let p = 1
+// for( let i=0; i<n; i++){
+// const x = +prompt('Son kiriting: ')
+// if(x<k && x!=0){
+//     console.log(x);
+//     summ+=1
+// }
+// // summ = (summ+(Math.floor(x))/10);
+// //p=(summ+x)*n
+// }
+
+//  console.log(summ);
+
+//15 masala
+let n = 5
+let k = 5
+let p
+for (let i = 1; i <= n; i++) {
+    const x = +prompt(`${i}-sonni kiriting: `)
+    if (x > k) {
+        p = x;
+        break;
+    }
+    console.log(p);
+}
+
+// console.log(summ);
