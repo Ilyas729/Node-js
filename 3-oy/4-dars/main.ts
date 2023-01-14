4444d78238569b64d7c7410ccf69c7b1b4936aed
mirzobeck <sobirovmirzobek3@gmail.com>
2023-01-14 12:10:20 +0500

    rtr

---
+++ 3-oy/4-dars/main.ts
@@ -0,0 +1,69 @@
+const arr: Array<string> = ['Asilbek','Aisha', 'Dilshod', 'Umidbek', 
+'Ilyosbek', 'Diyorbek', 'Shokirjon',
+'Akmalbek','Sanjarbek','Qudratbek']
+// const m: string[] = []
+ 
+// for(let i=0; i<=arr.length; i++){
+//   let a: string =arr.pop()! 
+//   m.push(a)
+// }
+// console.log(m);
+
+//array access operator
+//console.log(`${arr[0]} va ${arr[5]} Pubg oynashdi`);
+
+//array assigment operation
+// arr[2]='Dilshodbek'
+// console.log(arr);
+
+// arr.push()
+// console.log(arr);
+
+//let pupil: string = arr.shift()!
+// console.log(arr);
+// console.log(pupil);
+
+// arr.unshift(pupil!)
+// console.log(arr);
+
+//delete arr[2]
+//start, deleteCount
+// arr.splice(2, 2, "Gulshapshap")
+// console.log(arr);
+
+
+// for( let i=arr.length-1; i>=0; i--){
+//     console.log(arr[i]);
+    
+// }
+//2 masala
+// let m: string[]=arr.slice(0,5)
+// // arr.slice(1,5)
+// let n: string[]=arr.slice(6,10)
+
+// let a: string[]=m.concat(n)
+// console.log(a);
+// console.log(n);
+// console.log(m);
+
+//3 masala
+// let m: string[]=arr.slice(0,5)
+// // arr.slice(1,5)
+// let n: string[]=arr.slice(6,10)
+// let p: string[] = arr.slice(2, 5)
+
+// console.log(p);
+
+// let x = p.concat(m, n)
+// console.log(x);
+
+// // let a: string[]=m.concat(n)
+// //  console.log(a);
+// // console.log(n);
+// console.log(m);
+
+//4 masala
+
+const m: string[]=[arr[3],arr[4],arr[1],arr[7]]
+console.log(m);
+//5 masala
